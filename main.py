d9c932ccc7ecc8254e46f040d200d543945b1b6d
Riya Sharma <riyasharmaptn1717@gmail.com>
2026-06-16 20:33:51 +0530

    Build the foundation of a CLI AI chatbot and successfully make the first API call to an LLM.

--- main.py
+++ main.py
@@ -2,26 +2,18 @@ from dotenv import load_dotenv
 from groq import Groq
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 
 groq_api_key = os.getenv("GROQ_API_KEY")
-print(repr(groq_api_key))
-
-if not groq_api_key:
-    print("Error: GROQ_API_KEY not found.")
-    raise SystemExit
-
 
 client = Groq(api_key=groq_api_key)
 
-response = client.chat.completions.create( #Sends a chat completion request.
+response = client.chat.completions.create(
     model="llama-3.1-8b-instant",
-    messages=[
-        {
-            "role" : "user",
-            "content" : "Explain recursion in one sentence."
-        }
-    ]
+    messages=[{"role": "user", "content": "Explain recursion in one sentence."}]
 )
 
 print(response.choices[0].message.content)
+print("\nToken Usage:")
+print("Input Tokens:", response.usage.prompt_tokens)
+print("Output Tokens:", response.usage.completion_tokens)
