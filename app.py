9ca5d89381a0bef6929288083e579a3fa73a0bf0
Riya Sharma <riyasharmaptn1717@gmail.com>
2026-07-04 17:10:43 +0530

    implemented Fastapi-HTTPException

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 from providers.groq_provider import chat_with_groq
 from models.chat_models import ChatRequest, ChatResponse
-from fastapi.middleware.cors import CORSMiddleware
+
 
 messages = [
     {
@@ -13,23 +15,42 @@ messages = [
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware, #checkpoint for entry
+    CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
-    messages.append({"role": "user", "content": request.message})
 
-    bot_reply, input_tokens, output_tokens = chat_with_groq(
-        messages,
-        request.temperature
+    messages.append(
+        {
+            "role": "user",
+            "content": request.message
+        }
     )
 
-    messages.append({"role": "assistant", "content": bot_reply})
+    try:
+        bot_reply, input_tokens, output_tokens = chat_with_groq(
+            messages,
+            request.temperature
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="Unable to contact AI provider. Please try again later."
+        )
+
+    messages.append(
+        {
+            "role": "assistant",
+            "content": bot_reply
+        }
+    )
 
     return ChatResponse(
         reply=bot_reply,
@@ -38,21 +59,25 @@ def chat(request: ChatRequest):
         output_tokens=output_tokens
     )
 
+
 @app.get("/history")
 def get_history():
+
     return messages
-#GET /history
+
+
 @app.delete("/history")
 def clear_history():
+
     messages.clear()
 
     messages.append(
         {
-            "role" : "system",
-            "content" : "You are a helpful assistant"
+            "role": "system",
+            "content": "You are a helpful assistant"
         }
     )
 
     return {
-        "message" : "History cleared succesfully"
+        "message": "History cleared successfully."
     }
